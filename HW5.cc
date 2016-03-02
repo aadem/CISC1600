f01adeab586ce7485de1b0c1b20b0fc862e32e7d
Asha M. Adem <ashaadem@storm.cis.fordham.edu>
2016-03-02 14:27:14 -0500

    Assignment 5

---
+++ HW5.cc
@@ -0,0 +1,40 @@
+// input outpu Assignment 5.
+// Combine use loops & conditions.t 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+ int main()
+
+{
+
+ int guess, number = 0;
+ number = 10;
+ cout << "Guess a number between 1 and 20:\n";
+
+while ( guess != number )
+  {    
+    cin >> guess;
+ 
+if (guess > number)
+   {
+  cout << "Oops, too large!Guess again:\n";
+   }
+ else
+    if (guess < number)
+       {   
+         cout << "Oops, too small!Guess again:\n";
+       }
+     else
+        if ( guess == number )
+          { 
+             cout <<"Bingo! The number you guessed is correct." << endl;
+          }
+  
+  }
+
+return 0;
+
+}
+
